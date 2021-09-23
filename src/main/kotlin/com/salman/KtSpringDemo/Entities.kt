08a93381f9fa89136015ae16423b65886b02cc23
Salman A <salman.a@hatio.in>
2021-09-23 10:45:50 +0530

    Add entities for the project

---
+++ src/main/kotlin/com/salman/KtSpringDemo/Entities.kt
@@ -0,0 +1,7 @@
+package com.salman.KtSpringDemo
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
+
+@Table("MESSAGES")
+data class Message(@Id val id: String?, val text: String)
