4f1de9d3d622787e90af3396d344fc0960fe193d
Salman A <salman.a@hatio.in>
2021-09-23 10:47:31 +0530

    Add service file for the message repo

---
+++ src/main/kotlin/com/salman/KtSpringDemo/MessageService.kt
@@ -0,0 +1,13 @@
+package com.salman.KtSpringDemo
+
+import org.springframework.stereotype.Service
+
+@Service
+class MessageService(val db: MessageRepository) {
+
+    fun findMessages(): List<Message> = db.findMessages()
+
+    fun post(message: Message){
+        db.save(message)
+    }
+}
